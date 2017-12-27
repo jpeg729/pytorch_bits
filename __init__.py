0803a82758f09a60b6e59b452408e7b2672b352c
jpeg729 <jpeg729@gmail.com>
2017-12-27 10:59:05 +0100

    Correct __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,2 @@
 
-from .lstm import LSTM
-from .gru import GRU
-from .mgu import MGU, MGU2
-from .ran import RAN
-from .sru import SRUf, SRU2, SRU
-from .qrnn import fakeQRNN
-from .trnn import TRNN
-from .causal import CausalConv1d, Wave, ShortWave
+
